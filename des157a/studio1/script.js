e84edad722cc8259d93d0f070c969634926f2d57
Robert Campos <rcampos@ucdavis.edu>
2021-10-31 19:51:40 -0700

    Visual Thinking Analysis

--- des157a/studio1/script.js
+++ des157a/studio1/script.js
@@ -13,18 +13,18 @@
         const noun3 = document.querySelector('#noun3').value;
         const noun4 = document.querySelector('#noun4').value;
 
-        const myText = `This Year, I’m Going to dress up as a ${noun1}. My coustume is going to be so ${adj1}! It will be made with ${noun2} and ${noun3}, so its sure to win the ${noun4} contest! `;
+        const myText = `This Year, I’m Going to dress up as a ${noun1}. My costume is going to be so ${adj1}! It will be made with ${noun2} and ${noun3}, so its sure to win the ${noun4} contest! `;
 
         madlib.innerHTML = myText;
     })
 })();
 
-function on() {
-  document.getElementById("overlay").style.display = "block";
-}
+    function on() {
+    document.getElementById("overlay").style.display = "block";
+    }
 
-function off() {
-  document.getElementById("overlay").style.display = "none";
-}
+    function off() {
+    document.getElementById("overlay").style.display = "none";
+    }
 
 
